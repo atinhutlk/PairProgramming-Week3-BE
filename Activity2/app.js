239d6e9cfff20a8f926b21847b7217b84c7f5644
atinhutlk <nhut.vo@metropolia.fi>
2025-11-04 07:27:12 +0200

    Complete Week3-BE-PairProgramming

--- Activity2/app.js
+++ Activity2/app.js
@@ -2,43 +2,18 @@ const express = require("express");
 const app = express();
 
 const {
-<<<<<<< HEAD
-  getAllFeedbacks,
-  getFeedbackById,
-  createFeedback,
-  updateFeedback,
-  deleteFeedback,
-} = require("./feedbackHandlers");
-=======
   getAllTours,
   getTourById,
   createTour,
   updateTour,
   deleteTour,
 } = require("./tourHandlers"); 
->>>>>>> fe17221 (Complete Week 3 pair prgram Part2)
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // ROUTES
 
-<<<<<<< HEAD
-// GET /feedback
-app.get("/feedback", getAllFeedbacks);
-
-// POST /feedback
-app.post("/feedback", createFeedback);
-
-// GET /feedback/:feedbackId
-app.get("/feedback/:feedbackId", getFeedbackById);
-
-// PUT /feedback/:feedbackId
-app.put("/feedback/:feedbackId", updateFeedback);
-
-// DELETE /feedback/:feedbackId
-app.delete("/feedback/:feedbackId", deleteFeedback);
-=======
 // GET /tours
 app.get("/tours", getAllTours);
 
@@ -53,7 +28,6 @@ app.put("/tours/:tourId", updateTour);
 
 // DELETE /tours/:tourId
 app.delete("/tours/:tourId", deleteTour);
->>>>>>> fe17221 (Complete Week 3 pair prgram Part2)
 
 const port = 4000;
 // Start the server
